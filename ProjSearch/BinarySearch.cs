21937b04f2428772e2063deb81c20d52ccb0e0f3
Mayur Banta <mayurbanta@gmail.com>
2019-02-20 11:28:46 +0530

    Add project files.

---
+++ ProjSearch/BinarySearch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjSearch
+{
+    public class BinarySearch
+    {
+        public  int BinarySearchIterative(int[] arr, int x)
+        {
+            int start = 0;
+            int end = arr.Length - 1;
+
+            while (start <= end)
+            {
+                int mid = (start + end) / 2;
+
+                if (x < arr[mid])
+                {
+                    end = mid - 1;
+
+
+                }
+                else if (x > arr[mid])
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ ProjSearch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[6] { 45, 98, 2, 35, 9, 7 };
+            int[] sorteddarr = arr.OrderBy(i => i).ToArray();
+
+            BinarySearch binarySearch = new BinarySearch();
+            var a = binarySearch.BinarySearchIterative(sorteddarr, 7);
+
+        }
+
+        /// <summary>
+        /// a = array
+        /// x = what we are searching for
+        /// p = first index
+        /// q = mid point
+        /// r = last index
+        /// </summary>
+       
+
+
+       
+
+    }
+}
